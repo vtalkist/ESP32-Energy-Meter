e397417aa5282e5fd5ac0ef4cbaf8064ec28082e
Vitalii Klimov <vitaliy.inc.if.uk@gmail.com>
2023-05-03 21:05:58 +0300

    initial commit

---
+++ sketch_mar06a/sketch_mar06a.ino
@@ -0,0 +1,103 @@
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+#define BLYNK_PRINT Serial
+#include "EmonLib.h"
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+
+
+#define BLYNK_TEMPLATE_ID "TMPLEwNZlyNP"
+#define BLYNK_TEMPLATE_NAME "Data Monitoring"
+#define BLYNK_AUTH_TOKEN "3QwxM5mq366uxX-QMIGcvWVDw1-oSGnN"
+
+EnergyMonitor emon;
+#define vCalibration 154.4
+#define currCalibration 5
+
+BlynkTimer timer;
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+
+char ssid[] = "####"; // replace it with ur SSID
+char pass[] = "####"; // replace it with ur Wi-Fi key
+
+float kWh = 0;
+unsigned long lastmillis = millis();
+ 
+void myTimerEvent()
+{
+  emon.calcVI(20, 2000);
+  kWh = kWh + emon.apparentPower * (millis() - lastmillis) / 3600000000.0;
+  yield();
+  Serial.print("Vrms: ");
+  Serial.print(emon.Vrms, 2);
+  Serial.print("V");
+ 
+  Serial.print("\tIrms: ");
+  Serial.print(emon.Irms, 4);
+  Serial.print("A");
+ 
+  Serial.print("\tPower: ");
+  Serial.print(emon.apparentPower, 4);
+  Serial.print("W");
+ 
+  Serial.print("\tkWh: ");
+  Serial.print(kWh, 5);
+  Serial.println("kWh");
+  
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Vrms:");
+  lcd.print(emon.Vrms, 2);
+  lcd.print("V");
+  lcd.setCursor(0, 1);
+  lcd.print("Irms:");
+  lcd.print(emon.Irms, 4);
+  lcd.print("A");
+  delay(2500);
+  
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Power:");
+  lcd.print(emon.apparentPower, 4);
+  lcd.print("W");
+  lcd.setCursor(0, 1);
+  lcd.print("kWh:");
+  lcd.print(kWh, 4);
+  lcd.print("W");
+  delay(2500);
+ 
+  lastmillis = millis();
+ 
+  Blynk.virtualWrite(V0, emon.Vrms);
+  Blynk.virtualWrite(V1, emon.Irms);
+  Blynk.virtualWrite(V2, emon.apparentPower);
+  Blynk.virtualWrite(V3, kWh);
+}
+ 
+void setup()
+{
+  Serial.begin(115200);
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+  lcd.begin(16, 2);
+ 
+  emon.voltage(35, vCalibration, 1.7); // Voltage: input pin, calibration, phase_shift
+  emon.current(34, currCalibration); // Current: input pin, calibration.
+ 
+  timer.setInterval(5000L, myTimerEvent);
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(3, 0);
+  lcd.print("IoT Energy");
+  lcd.setCursor(5, 1);
+  lcd.print("Meter");
+  delay(3000);
+  lcd.clear();
+}
+ 
+void loop()
+{
+  Blynk.run();
+  timer.run();
+}
